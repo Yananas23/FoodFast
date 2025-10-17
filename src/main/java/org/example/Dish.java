dd997b08a26b4f63e2fab0665bea4b851ef7ce4f
Yananas23 <118125780+Yananas23@users.noreply.github.com>
2025-10-17 12:26:21 +0200

    Create Dish.java

---
+++ src/main/java/org/example/Dish.java
@@ -0,0 +1,43 @@
+import java.math.BigDecimal;
+import java.util.Objects;
+
+
+public class Dish {
+    private String name;
+    private BigDecimal price;
+    private DishSize size;
+
+    public enum DishSize {
+        SMALL, MEDIUM, LARGE
+    }
+
+    public Dish(String name, BigDecimal price, DishSize size) {
+        this.name = name;
+        this.price = price;
+        this.size = size;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Dish)) return false;
+        Dish dish = (Dish) o;
+        return Objects.equals(name, dish.name) &&
+                Objects.equals(price, dish.price) &&
+                size == dish.size;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, price, size);
+    }
+
+    @Override
+    public String toString() {
+        return "Dish{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                ", size=" + size +
+                '}';
+    }
+}
