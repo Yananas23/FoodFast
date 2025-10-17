621a31958041fac1951eb76075ef2fb1c9e0d271
Nanotek <matheo.lancea@gmail.com>
2025-10-17 12:39:56 +0200

    ajout de Customer

---
+++ src/main/java/org/example/Customer.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import java.util.Objects;
+
+public class Customer {
+    private String id;
+    private String name;
+    private String address;
+
+    public Customer(String id, String name, String address) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Customer)) return false;
+        Customer customer = (Customer) o;
+        return Objects.equals(id, customer.id) &&
+                Objects.equals(name, customer.name) &&
+                Objects.equals(address, customer.address);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, address);
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
