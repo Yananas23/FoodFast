8728fa66599392684c7cead39ee608f3031a09df
Yananas23 <118125780+Yananas23@users.noreply.github.com>
2025-11-21 15:14:07 +0100

    sumUpTo

--- src/main/java/org/example/FoodFastUtils.java
+++ src/main/java/org/example/FoodFastUtils.java
@@ -28,7 +28,11 @@ public class FoodFastUtils {
     }
 
     static int sumUpTo(int n) {
-        return(n*(n+1)/2);
+        int result = 0;
+        for (int i = 1; i <= n; i++){
+            result += i;
+        }
+        return result;
     }
 
     static String anonymize(String text) {
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-
-        Application.main(new String[]{"test"});
+        System.out.println(FoodFastUtils.sumUpTo(5));
     }
 }
