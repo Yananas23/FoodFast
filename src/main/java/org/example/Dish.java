9f2377587ed1f4424d81a7b229619cc531168c10
Yananas23 <118125780+Yananas23@users.noreply.github.com>
2025-11-21 16:34:08 +0100

    Order.java
    ouai

--- src/main/java/org/example/Dish.java
+++ src/main/java/org/example/Dish.java
@@ -8,6 +8,30 @@ public class Dish {
     private BigDecimal price;
     private DishSize size;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public DishSize getSize() {
+        return size;
+    }
+
+    public void setSize(DishSize size) {
+        this.size = size;
+    }
+
     public enum DishSize {
         SMALL, MEDIUM, LARGE
     }
--- src/main/java/org/example/Order.java
+++ src/main/java/org/example/Order.java
@@ -1,4 +1,34 @@
 package org.example;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.UUID;
+
 public class Order {
+    private String id;
+    private OrderStatus status;
+    private Map<Dish, Integer> dishes;
+    private Customer customer;
+    private LocalDateTime orderDate;
+
+    public enum OrderStatus {
+        PENDING, IN_PREPARATION, COMPLETED, CANCELLED
+    }
+
+    public Order(OrderStatus status, Map<Dish, Integer> dishes, Customer customer) {
+        this.id = UUID.randomUUID().toString();
+        this.status = status;
+        this.dishes = dishes;
+        this.customer = customer;
+        this.orderDate = LocalDateTime.now();
+    }
+
+    public BigDecimal calculateTotalPrice() {
+        return dishes.entrySet().stream()
+                .map(entry -> entry.getKey().getPrice()
+                .multiply(BigDecimal.valueOf(entry.getValue())))
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+
 }
