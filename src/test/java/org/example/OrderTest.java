690eb44be435641fb78609caf1fa1411f1b16ab3
Red <timeo2six@gmail.com>
2025-12-02 11:38:29 +0100

    Update OrderTest.java

--- src/test/java/org/example/OrderTest.java
+++ src/test/java/org/example/OrderTest.java
@@ -5,6 +5,11 @@ import org.junit.jupiter.api.Test;
 public class OrderTest {
     @Test
     public void testGetter() {
+        Order order = new Order();
+        order.setId(1);
+        order.setDescription("Test Order");
 
+        assert order.getId() == 1;
+        assert order.getDescription().equals("Test Order");
     }
 }
