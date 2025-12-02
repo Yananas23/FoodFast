418b1ae335cbc77ffcfb4dc2a9d05e06cb4f89b6
Yananas23 <118125780+Yananas23@users.noreply.github.com>
2025-12-02 12:16:04 +0100

    test partie 126789

---
+++ src/test/java/org/example/DishTest.java
@@ -0,0 +1,46 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+public class DishTest {
+    @Test
+    public void testGetter() {
+
+        Dish order = new Order(Order.OrderStatus.PENDING, null, null);
+
+        assert order.getId() != null;
+        assert order.getStatus() == Order.OrderStatus.PENDING;
+        assert order.getDishes() == null;
+        assert order.getCustomer() == null;
+        assert order.getOrderDate() != null;
+    }
+
+    @Test
+    public void testEquals() {
+        Order order1 = new Order(Order.OrderStatus.PENDING, null, null);
+        Order order2 = new Order(Order.OrderStatus.PENDING, null, null);
+
+        assert !order1.equals(order2);
+    }
+
+    @Test
+    public void testHashCode() {
+        Order order1 = new Order(Order.OrderStatus.PENDING, null, null);
+        Order order2 = new Order(Order.OrderStatus.PENDING, null, null);
+
+        assert order1.hashCode() != order2.hashCode();
+    }
+
+    @Test
+    public void testToString() {
+        Order order = new Order(Order.OrderStatus.PENDING, null, null);
+        String toString = order.toString();
+
+        assert toString.contains("Order");
+        assert toString.contains("id='");
+        assert toString.contains("status=");
+        assert toString.contains("dishes=");
+        assert toString.contains("customer=");
+        assert toString.contains("orderDate=");
+    }
+}
