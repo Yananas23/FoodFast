82ea9135e0b47b1acd278563621232fbb5d8c6ee
Red <timeo2six@gmail.com>
2026-01-06 09:49:54 +0100

    Update DeliveryPlatform.java

--- src/main/java/org/example/DeliveryPlatform.java
+++ src/main/java/org/example/DeliveryPlatform.java
@@ -1,7 +1,10 @@
 package org.example;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+
+import org.example.Order.OrderStatus;
 
 public class DeliveryPlatform {
      private Map<String, Order> orders;
@@ -12,5 +15,17 @@ public class DeliveryPlatform {
 
      public Optional<Order> findOrderById(String orderId) {
          return Optional.ofNullable(orders.get(orderId));
+     }
+
+     public List<Order> findOrdersByCustomer(Customer customer) {
+            return orders.values().stream()
+                    .filter(order -> order.getCustomer().equals(customer))
+                    .toList();
+     }
+
+     public List<Order> findOrdersByStatus(OrderStatus status) {
+            return orders.values().stream()
+                    .filter(order -> order.getStatus() == status)
+                    .toList();
      }
 }
