9d0ebd207059f6cb87fbfe9f0b3f022269d9a7b9
Red <timeo2six@gmail.com>
2026-01-06 09:45:00 +0100

    Update DeliveryPlatform.java

--- src/main/java/org/example/DeliveryPlatform.java
+++ src/main/java/org/example/DeliveryPlatform.java
@@ -1,4 +1,16 @@
 package org.example;
 
+import java.util.Map;
+import java.util.Optional;
+
 public class DeliveryPlatform {
+     private Map<String, Order> orders;
+
+     public void placeOrder(Order order) {
+         orders.put(order.getId(), order);
+     }
+
+     public Optional<Order> findOrderById(String orderId) {
+         return Optional.ofNullable(orders.get(orderId));
+     }
 }
